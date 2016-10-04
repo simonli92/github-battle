1a07d14cd308a1c01476626ba3f06eaf7d781457
jingdong li <jingdonl@gmail.com>
2016-10-04 14:16:24 -0400

    github-battle

---
+++ app/components/Main.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
+
+var Main = React.createClass({
+	render:function(){
+		return (
+			<div className='main-container'>
+			  <ReactCSSTransitionGroup
+			    transitionName='appear'
+			    transitionEnterTimeout={500}
+			    transitionLeaveTimeout={500}>
+			  	  {this.props.children}
+			  </ReactCSSTransitionGroup>
+			</div>
+		)
+	}
+});
+
+module.exports = Main;
---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,49 @@
+var React = require ('react');
+var Prompt = require('../components/Prompt');
+
+var PromptContainer = React.createClass({
+	contextTypes:{
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState: function(){
+		return{
+			username:''
+		}
+	},
+	handleUpdateUser: function(e){
+		this.setState({
+			username: e.target.value
+		})
+	},
+	handleSubmitUser: function(e){
+		e.preventDefault();
+		var username=this.state.username;
+		this.setState({
+			username:''
+		});
+		if (this.props.routeParams.playerOne) {
+		  this.context.router.push({
+		  	pathname: '/battle',
+		  	query:{
+		  		playerOne: this.props.routeParams.playerOne,
+		  		playerTwo: this.state.username
+		  	}
+		  })
+              //go to /battle
+		} else {
+		  this.context.router.push('/playerTwo/'+this.state.username)
+			//go to /playerTwo
+		}
+	},
+	render: function(){
+	  return (
+	  	<Prompt 
+	  	  onSubmitUser={this.handleSubmitUser}
+	  	  onUpateUser={this.handleUpdateUser}
+	  	  header={this.props.route.header}
+	  	  username={this.state.username} />
+	  )
+	}
+});
+
+module.exports = PromptContainer;
